ed7e27027612a9a8675d06912574085c10005950
Pasha Terekhov <p.terehov@purpleplane-it.com>
2023-09-20 19:42:27 +0300

    add response model

---
+++ RickAndMorty/Core/BaseViewController.swift
@@ -0,0 +1,14 @@
+//
+//  BaseViewController.swift
+//  RickAndMorty
+//
+//  Created by Чебупелина on 20.09.2023.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ RickAndMorty/Modules/CharactesListModule/Model/CharactersListModel.swift
@@ -0,0 +1,45 @@
+//
+//  CharactersListModel.swift
+//  RickAndMorty
+//
+//  Created by Чебупелина on 20.09.2023.
+//
+
+import Foundation
+
+struct CharactersListResponse: Decodable {
+    let info: CharactersListInfo
+    let results: [Character]
+}
+
+struct CharactersListInfo: Decodable {
+    let count: Int
+    let pages: Int
+    let next: URL?
+    let prev: URL?
+}
+
+struct Character: Decodable, Identifiable {
+    let id: Int
+    let name: String
+    let status: String
+    let species: String
+    let type: String
+    let gender: String
+    let origin: CharacterOrigin
+    let location: CharacterLocation
+    let image: URL?
+    let episode: [URL]
+    let url: URL
+    let created: Date
+}
+
+struct CharacterOrigin: Decodable {
+    let name: String
+    let url: URL
+}
+
+struct CharacterLocation: Decodable {
+    let name: String
+    let url: URL
+}
